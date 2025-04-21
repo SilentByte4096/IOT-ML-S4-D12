594ec725a9b3d8e71e3221ac49cccb3cda8166c5
Neha R Menon <neharmenon05@gmail.com>
2025-04-21 08:20:11 +0530

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 1_signal_generation.py  # Generate signals
-python3 2_feature_extraction.py  # Extract features
-python3 3_preprocess.py  # Preprocess data
-python3 4_train_model.py  # Train the model
-python3 5_test.py  # Test the model
-python3 6_visualize.py  # Visualize results
-python3 7_verify.py  # Verify accuracy and save results
-
